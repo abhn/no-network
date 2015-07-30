aca7fb580999517fafd6dff1f8871f8fd8098af6
Abhishek Nagekar <abhishek@nagekar.com>
2015-07-30 23:08:25 +0530

    new view

--- api/views/vNew.py
+++ api/views/vNew.py
@@ -37,7 +37,7 @@ def newComment(request):
             app_id = nComment['app_id']
             if(models.ApiAccessToken.objects.all().filter(access_token=access_t).exists()):
                 db_access = models.ApiAccessToken.objects.get(access_token=access_t)
-                db_comments.comment_source_id_id = db_access.user_id
+                db_comments.comment_source_id = db_access.user_id
                 db_comments.save()
                 return HttpResponse(json.dumps({"message":"successfully created comment"}), content_type="application/json")
             else:
@@ -51,12 +51,12 @@ def newStar(request):
             nStar = json.loads(request.body)
             db_star = models.ApiStars()
             db_star.star_updated = None
-            db_star.star_destination_id_id = nStar['destination_id']
+            db_star.star_destination_id = nStar['destination_id']
             access_t = nComment['access_token']
             app_id = nComment['app_id']
             if(models.ApiAccessToken.objects.all().filter(access_token=access_t).exists()):
                 db_access = models.ApiAccessToken.objects.get(access_token=access_t)
-                db_star.star_source_id_id = db_access.user_id
+                db_star.star_source_id = db_access.user_id
                 db_star.save()
                 return HttpResponse(json.dumps({"message":"successfully stared"}), content_type="application/json")
             else:
@@ -70,5 +70,17 @@ def newRecommend(request):
             nRecommend = json.loads(request.body)
             db_recommend = models.ApiRecommends()
             db_recommend.recommend_updated = None
+            db_recommend.recommend.recommend_destination_id = nRecommend['destination_id']
+            access_t = nComment['access_token']
+            app_id = nComment['app_id']
+            if(models.ApiAccessToken.objects.all().filter(access_token=access_t).exists()):
+                db_access = models.ApiAccessToken.objects.get(access_token=access_t)
+                db_recommend.recommend_source_id = db_access.user_id
+                db_recommend.save()
+                return HttpResponse(json.dumps({"message":"successfully recommended"}), content_type="application/json")
+            else:
+                return HttpResponse(json.dumps({"error":"authentication required to perform this action"}), content_type="application/json")
+    else:
+        return HttpResponse(json.dumps({"error":"POST request expected"}), content_type="application/json")
             
             
